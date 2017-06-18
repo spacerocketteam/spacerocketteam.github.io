4c2ed840a389075d849c6f124eb4322f93752c5b
Kevin Oliveira <kevin.oliveira@live.com>
2017-06-18 15:51:47 -0300

    Ultimas modificações

--- js/custom.js
+++ js/custom.js
@@ -15,26 +15,5 @@ function phoneScreen(appname) {
 };
 
 
-function mailPOST() {
-    // $(".test").modal('show','closable', true);/*
-    var params = 'nome=' + document.getElementById('name').value + '&email=' + document.getElementById('email').value + '&mensagem=' + document.getElementById('message').value;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("modalmail").innerHTML = this.responseText;
-            document.getElementById('name').value = "";
-            document.getElementById('email').value = "";
-            document.getElementById('message').value = "";
-            $(".test").modal('show', 'closable', true);
-        }
-    };
-    xmlhttp.open("POST", "mail.php", true);
-
-    //Send the proper header information along with the request
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    xmlhttp.send(params);
-    //*/
-};
 
 
